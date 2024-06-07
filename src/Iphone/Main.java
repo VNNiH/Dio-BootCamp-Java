37862a6ea709d649fbc3a6a8f505d8812b266dfa
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-06-07 15:14:49 -0300

    Update Main.java

--- src/Iphone/Main.java
+++ src/Iphone/Main.java
@@ -2,17 +2,17 @@ package Iphone;
 
 public class Main {
     public static void main(String[] args) {
-        Iphone iphone1 = new Iphone("Vinicius Celular");
+        Iphone iphone1 = new Iphone("Celular");
         //REPRODUÇÃO MUSICAL
         iphone1.tocar();
         iphone1.selecionarMusica("Only Exception");
         iphone1.pausar();
         //APARELHO TELEFONICO
-        iphone1.ligar("91 981655616");
+        iphone1.ligar("91 9999999");
         iphone1.atender();
         iphone1.iniciarCorreioVoz();
         //NAVEGAR NA INTERNET
-        iphone1.exibirPagina("https://github.com/VNNiH");
+        iphone1.exibirPagina("https://github.com");
         iphone1.adicionarNovaAba();
         iphone1.atualizarPagina();
     }
