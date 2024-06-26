20a775000bc5d80dcf1bdd0a45ba49059e7731d7
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-06-26 20:49:13 -0300

    Create Facebook.java

---
+++ src/pilaresPOO/mensagens/Facebook.java
@@ -0,0 +1,10 @@
+package pilaresPOO.mensagens;
+
+public class FacebookMessenger extends ServicoComum {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook");
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook");
+    }
+}
