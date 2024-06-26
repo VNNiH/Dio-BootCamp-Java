045005961ff030a872d96d9277fa27ee729a6e25
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-06-26 20:49:32 -0300

    Create MSN.java

---
+++ src/pilaresPOO/mensagens/MSN.java
@@ -0,0 +1,10 @@
+package pilaresPOO.mensagens;
+
+public class MSNMessenger extends ServicoComum{
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo MSN Messenger");
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN Messenger");
+    }
+}
